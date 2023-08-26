26d37a5a4ba3026f3b0c84020507072ec0db0ae7
FanFusheng <ffs@ppno.net>
2023-08-26 13:21:15 +0800

    test(net): 增加单元测试
    Signed-off-by: FanFusheng <ffs@ppno.net>

--- net/ip_test.go
+++ net/ip_test.go
@@ -1,6 +1,7 @@
 package net
 
 import (
+	"fmt"
 	"net"
 	"testing"
 
@@ -8,8 +9,13 @@ import (
 )
 
 type (
-	testCase struct {
+	testIPCase struct {
 		ip  net.IP
+		ret bool
+	}
+	testNetCase struct {
+		ip  net.IP
+		net net.IPNet
 		ret bool
 	}
 )
@@ -17,8 +23,9 @@ type (
 func TestIsIPv4Public(t *testing.T) {
 	assertions := assert.New(t)
 
-	cases := []testCase{
+	cases := []testIPCase{
 		{net.IPv4(192, 168, 0, 1), false},
+		{net.IPv4(120, 27, 53, 19), true},
 	}
 
 	for _, c := range cases {
@@ -28,4 +35,89 @@ func TestIsIPv4Public(t *testing.T) {
 		})
 	}
 
+}
+
+func TestIsIPv4(t *testing.T) {
+	assertions := assert.New(t)
+
+	cases := []testIPCase{
+		{net.IPv4(192, 168, 0, 1), true},
+		{net.IPv4(120, 27, 53, 19), true},
+	}
+
+	for _, c := range cases {
+		t.Run(c.ip.String(), func(t *testing.T) {
+			r := IsIPv4(c.ip)
+			assertions.Equal(c.ret, r)
+		})
+	}
+}
+
+func TestIsIPv4Inner(t *testing.T) {
+	assertions := assert.New(t)
+
+	cases := []testIPCase{
+		{net.IPv4(192, 168, 0, 1), true},
+		{net.IPv4(120, 27, 53, 19), false},
+	}
+
+	for _, c := range cases {
+		t.Run(c.ip.String(), func(t *testing.T) {
+			r := IsIPv4Inner(c.ip)
+			assertions.Equal(c.ret, r)
+		})
+	}
+}
+
+func TestContains(t *testing.T) {
+	assertions := assert.New(t)
+
+	cases := []testNetCase{
+		{net.IPv4(192, 168, 0, 1), inner010, false},
+		{net.IPv4(192, 168, 0, 1), inner100064, false},
+		{net.IPv4(192, 168, 0, 1), inner172016, false},
+		{net.IPv4(192, 168, 0, 1), inner192168, true},
+		{net.IPv4(120, 27, 53, 19), inner010, false},
+		{net.IPv4(120, 27, 53, 19), inner100064, false},
+		{net.IPv4(120, 27, 53, 19), inner172016, false},
+		{net.IPv4(120, 27, 53, 19), inner192168, false},
+		{net.IPv4(120, 27, 53, 18), inner000, false},
+		{net.IPv4(120, 27, 53, 18), inner127, false},
+		{net.IPv4(120, 27, 53, 18), inner169254, false},
+		{net.IPv4(120, 27, 53, 18), inner192000, false},
+		{net.IPv4(120, 27, 53, 18), inner192000002, false},
+		{net.IPv4(120, 27, 53, 18), inner192088099, false},
+		{net.IPv4(120, 27, 53, 18), inner198018, false},
+		{net.IPv4(120, 27, 53, 18), inner198051100, false},
+		{net.IPv4(120, 27, 53, 18), inner203000113, false},
+		{net.IPv4(120, 27, 53, 18), inner224, false},
+		{net.IPv4(120, 27, 53, 18), inner240, false},
+		{net.IPv4(120, 27, 53, 18), inner255, false},
+	}
+
+	for _, c := range cases {
+		name := fmt.Sprintf("%[1]s in %[2]s %[3]v", c.ip.String(), c.net.String(), c.ret)
+		t.Run(name, func(t *testing.T) {
+			r := c.net.Contains(c.ip)
+			assertions.Equal(c.ret, r)
+		})
+	}
+}
+
+func TestIsGlobalUnicast(t *testing.T) {
+	assertions := assert.New(t)
+
+	// 全局单播地址参考材料：https://developer.aliyun.com/article/406115。
+
+	cases := []testIPCase{
+		{net.IPv4(192, 168, 0, 1), true},
+		{net.IPv4(120, 27, 53, 19), true},
+	}
+
+	for _, c := range cases {
+		t.Run(c.ip.String(), func(t *testing.T) {
+			r := c.ip.IsGlobalUnicast()
+			assertions.Equal(c.ret, r)
+		})
+	}
 }
