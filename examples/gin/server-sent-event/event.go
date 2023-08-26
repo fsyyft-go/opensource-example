c9a8bcc441d05278698822ca1f113874f49544e4
FanFusheng <ffs@ppno.net>
2023-08-26 14:18:22 +0800

    feat: 增加使用 gin 实现 SSE 消息推送示例
    - 抽象 Event 结构体
    
    Signed-off-by: FanFusheng <ffs@ppno.net>

---
+++ examples/gin/server-sent-event/event.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	contextClientChanName = "clientChan"
+)
+
+type (
+	// ClientChan 将新事件消息广播到所有已注册的客户端连接通道。
+	ClientChan chan string
+
+	// event 事件管理器。
+	// 保留当前附加的客户机列表，并向这些客户机广播事件。
+	event struct {
+		// Message 要发送的消息都提交到这个通道。
+		Message chan string
+
+		// NewClients 新创建的连接。
+		NewClients chan chan string
+
+		// ClosedClients 关闭的连接。
+		ClosedClients chan chan string
+
+		// TotalClients 所有客户端。
+		TotalClients map[chan string]bool
+	}
+)
+
+func (e *event) serveHTTP() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 初始化客户端连接通道。
+		clientChan := make(ClientChan)
+
+		// 发送新创建的连接消息。
+		e.NewClients <- clientChan
+
+		defer func() {
+			// 发送关闭的连接消息。
+			e.ClosedClients <- clientChan
+		}()
+
+		c.Set(contextClientChanName, clientChan)
+
+		c.Next()
+	}
+}
+
+// listen 监听所有消息。
+// 客户端的传入、添加或删除客户端、广播消息。
+func (e *event) listen(ctx context.Context) {
+LOOP:
+	for {
+		select {
+		// 添加活跃连接。
+		case client := <-e.NewClients:
+			e.TotalClients[client] = true
+			log.Printf("Client added. %d registered clients", len(e.TotalClients))
+
+		// 移除客户端。
+		case client := <-e.ClosedClients:
+			delete(e.TotalClients, client)
+			close(client)
+			log.Printf("Removed client. %d registered clients", len(e.TotalClients))
+
+		// 向所有客户端广播。
+		case eventMsg := <-e.Message:
+			for clientMessageChan := range e.TotalClients {
+				clientMessageChan <- eventMsg
+			}
+		case <-ctx.Done():
+			break LOOP
+		}
+
+	}
+}
--- examples/gin/server-sent-event/main.go
+++ examples/gin/server-sent-event/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
-	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,25 +15,6 @@ const (
 
 	addr = "127.0.0.1:8085"
 )
-
-// It keeps a list of clients those are currently attached
-// and broadcasting events to those clients.
-type Event struct {
-	// Events are pushed to this channel by the main events-gathering routine
-	Message chan string
-
-	// New client connections
-	NewClients chan chan string
-
-	// Closed client connections
-	ClosedClients chan chan string
-
-	// Total client connections
-	TotalClients map[chan string]bool
-}
-
-// New event messages are broadcast to all registered client connection channels
-type ClientChan chan string
 
 func main() {
 	router := gin.Default()
@@ -86,61 +67,17 @@ func main() {
 }
 
 // Initialize event and Start procnteessing requests
-func NewServer() (event *Event) {
-	event = &Event{
+func NewServer() *event {
+	e := &event{
 		Message:       make(chan string),
 		NewClients:    make(chan chan string),
 		ClosedClients: make(chan chan string),
 		TotalClients:  make(map[chan string]bool),
 	}
 
-	go event.listen()
+	go e.listen(context.TODO())
 
-	return
-}
-
-// It Listens all incoming requests from clients.
-// Handles addition and removal of clients and broadcast messages to clients.
-func (stream *Event) listen() {
-	for {
-		select {
-		// Add new available client
-		case client := <-stream.NewClients:
-			stream.TotalClients[client] = true
-			log.Printf("Client added. %d registered clients", len(stream.TotalClients))
-
-		// Remove closed client
-		case client := <-stream.ClosedClients:
-			delete(stream.TotalClients, client)
-			close(client)
-			log.Printf("Removed client. %d registered clients", len(stream.TotalClients))
-
-		// Broadcast message to client
-		case eventMsg := <-stream.Message:
-			for clientMessageChan := range stream.TotalClients {
-				clientMessageChan <- eventMsg
-			}
-		}
-	}
-}
-
-func (stream *Event) serveHTTP() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// Initialize client channel
-		clientChan := make(ClientChan)
-
-		// Send new connection to event server
-		stream.NewClients <- clientChan
-
-		defer func() {
-			// Send closed connection to event server
-			stream.ClosedClients <- clientChan
-		}()
-
-		c.Set("clientChan", clientChan)
-
-		c.Next()
-	}
+	return e
 }
 
 func HeadersMiddleware() gin.HandlerFunc {
