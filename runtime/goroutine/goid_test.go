6e9e41b2132672c5e512f4fde24539e6dcfb306d
FanFusheng <ffs@ppno.net>
2023-08-26 13:16:08 +0800

    feat: 增加获取 goroutine ID 方法
    Signed-off-by: FanFusheng <ffs@ppno.net>

---
+++ runtime/goroutine/goid_test.go
@@ -0,0 +1,29 @@
+package goroutine
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetGoID(t *testing.T) {
+	assertions := assert.New(t)
+	t.Run("测试获取 GoroutineID", func(t *testing.T) {
+
+		var wg sync.WaitGroup
+		var idOuter, idInternal int64
+		wg.Add(1)
+		idOuter = GetGoID()
+		go func() {
+			idInternal = GetGoID()
+			wg.Done()
+		}()
+		wg.Wait()
+		// 值每次都不一样，有需要的情况可以打印出来查看。
+		assertions.NotEqual(idOuter, idInternal)
+		// 在没有复用的情况下，里的一般会比外的大。
+		assertions.LessOrEqual(idOuter, idInternal)
+		// fmt.Println(idInternal, idOuter)
+	})
+}
